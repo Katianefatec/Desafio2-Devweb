4149c927dacb9237fa4f1b8353845d8d7655c18a
katiane <katiane.soares@fatec.sp.gov.br>
2023-04-13 11:08:49 -0300

    modificações de estilo e fundo

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\katia\OneDrive\Desktop\desafio2-devweb\venv
+set VIRTUAL_ENV=C:\Users\Fatec\Desktop\devweb\Desafio2-Devweb\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
